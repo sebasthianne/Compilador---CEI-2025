4a72d7cbc575412b7edcc5395360cb34a526ce42
sebasthianne <sebasthianne03@gmail.com>
2025-08-28 14:26:08 -0300

    Cliente Lexico

--- src/lexicalClient/LexicalClient.java
+++ src/lexicalClient/LexicalClient.java
@@ -1,6 +1,11 @@
 package lexicalClient;
 
+import domain.Token;
 import lexicalAnalyzer.LexicalAnalyzer;
+import lexicalAnalyzer.lexicalExceptions.LexicalException;
+import sourcemanager.SourceManager;
+
+import java.io.IOException;
 
 public class LexicalClient {
     LexicalAnalyzer lexicalAnalyzer;
@@ -9,8 +14,26 @@ public class LexicalClient {
         lexicalAnalyzer= null;
     }
 
-    public void executeLexicalAnalysis(LexicalAnalyzer analyzer){
+    public void executeLexicalAnalysis(LexicalAnalyzer analyzer) throws IOException {
         lexicalAnalyzer= analyzer;
+        Token currentToken = new Token("","",0);
+        try {
+            while (!currentToken.lexeme().equals("" + SourceManager.END_OF_FILE)) {
+                currentToken = analyzer.getNextToken();
+                System.out.println(currentToken);
+            }
+            System.out.println();
+            System.out.println("[SinErrores]");
+        } catch (LexicalException e) {
+            handleLexicalException(e);
+        }
+    }
+
+    public void handleLexicalException(LexicalException exception){
+        System.out.println(exception.getErrorData());
+        System.out.println("Detalle: "+ exception.getErrorDetail());
+        System.out.println();
+        System.out.println("[Error:"+exception.getErrorData().lexeme()+"|"+exception.getErrorData().lineNumber()+"]");
     }
 
     //TODO: Implement client for the lexical analyzer
